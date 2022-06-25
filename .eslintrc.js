53dcc6703606b4f37c4d3276b3f766bc03440bee
qijinyang <1772113523@qq.com>
2022-06-25 19:15:47 +0800

    perf(git): 添加了cz大法

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  
   root: true,
   env: {
     node: true
@@ -9,8 +10,15 @@ module.exports = {
   ],
   parserOptions: {
     parser: 'babel-eslint'
+    
   },
+//   "quotes": "error", // 默认
+
+// "quotes": "warn", // 修改为警告
+
+// "quotes": "off" ,// 修改不校验
   rules: {
+    'space-before-function-paren': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
