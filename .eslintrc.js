86b321ab9a20f9b1e687f8db81c51e44fabd9d05
qijinyang <1772113523@qq.com>
2022-06-26 19:43:25 +0800

    feat(git): commitlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,17 +9,28 @@ module.exports = {
     '@vue/standard'
   ],
   parserOptions: {
-    parser: 'babel-eslint'
-    
+    parser: 'babel-eslint',
+    requireConfigFile: false
   },
 //   "quotes": "error", // 默认
-
 // "quotes": "warn", // 修改为警告
-
 // "quotes": "off" ,// 修改不校验
   rules: {
     'space-before-function-paren': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-tabs': 'warn',
+    'vue/multi-word-component-names': 'off'
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
