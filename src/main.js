c6898db20b825d2807cb920f1ecc1574d10e1f93
qijinyang <1772113523@qq.com>
2022-06-27 16:45:41 +0800

    feat(封装axios、api、环境变量、跨域、loading、统一传参): 封装axios、api、环境变量、跨域、loading、统一传参

--- src/main.js
+++ src/main.js
@@ -6,7 +6,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.scss'
+import './iconfont/iconfont.css'
+import installIcons from './icons/index.js'
 
 const app = createApp(App)
+installIcons(app)
+
+console.log(process.env.VUE_APP_BASE_API)
 
 app.use(store).use(router).use(ElementPlus).mount('#app')
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,70 +1,50 @@
 <template>
   <div class="login-container">
-    <el-form
-      ref="LoginForm"
-      :model="loginForm"
-      :rules="loginRules"
-      class="login-form"
-    >
+    <el-form ref="LoginForm" :model="loginForm" :rules="loginRules" class="login-form">
       <div class="title-container">
         <h3 class="title">用户登录</h3>
-        <span class="icon">icon</span>
+        <svg-icon className="svg-language" icon="language"></svg-icon>
       </div>
       <el-form-item prop="username">
         <span class="svg-container">
           <el-icon>
-            <avatar />
+             <svg-icon icon="user"></svg-icon>
           </el-icon>
         </span>
-        <el-input v-model="loginForm.username" />
+        <el-input v-model.trim="loginForm.username" />
       </el-form-item>
       <el-form-item prop="password">
-        <el-input :type="inputType" v-model="loginForm.password">
-          <template #prefix>
-            <el-icon class="el-input__icon"><search /></el-icon>
-          </template>
-          <template #suffix>
-            <el-icon
-              v-if="inputType === 'password'"
-              @click="handllePassWordStatus"
-              class="el-input__icon"
-              ><Hide
-            /></el-icon>
-            <el-icon
-              v-else
-              @click="handllePassWordStatus"
-              class="el-input__icon"
-              ><View
-            /></el-icon>
-          </template>
-        </el-input>
+        <span class="svg-container">
+          <el-icon>
+             <svg-icon icon="password"></svg-icon>
+          </el-icon>
+        </span>
+        <el-input :type="inputType" v-model.trim="loginForm.password"></el-input>
+        <span class="svg-pwd" @click="handllePassWordStatus">
+          <el-icon>
+            <svg-icon :icon="passwordIconStatus"></svg-icon>
+          </el-icon>
+        </span>
       </el-form-item>
-      <el-button
-        class="login-button"
-        type="primary"
-        @click="handleLoginSubmit(LoginForm)"
-        >登录</el-button
-      >
-      <el-button
-        class="login-button"
-        type="primary"
-        @click="handleLoginSubmit(LoginForm)"
-        >登录</el-button
-      >
+      <el-button  class="login-button" type="primary" @click="handleLoginSubmit">登录</el-button>
     </el-form>
   </div>
 </template>
+
 <script setup>
-import { reactive, ref } from 'vue'
+import UserApi from '../../api/user'
+import { reactive, ref, computed } from 'vue'
 import { validatePassword } from './rule'
-import { Avatar, Search, View, Hide } from '@element-plus/icons-vue'
+import md5 from 'md5'
 
 const inputType = ref('password')
+const LoginForm = ref()
 
 const loginForm = reactive({
-  username: 'admin',
+  username: 'super-admin',
   password: '123456'
 })
+
 const loginRules = reactive({
   username: [
     {
@@ -82,98 +62,127 @@ const loginRules = reactive({
   ]
 })
 
-const handleLoginSubmit = async (formName) => {
-  if (!formName) return
-  await formName.validate((valid) => {
+const passwordIconStatus = computed(() => {
+  return inputType.value === 'password' ? 'eye' : 'eye-open'
+})
+
+/**
+ * 登录方式
+ */
+const handleLoginSubmit = async () => {
+  if (!LoginForm.value) return
+  await LoginForm.value.validate(async valid => {
     if (valid) {
       alert('登录')
+      loginForm.password = md5(loginForm.password)
+      const response = await UserApi.login(loginForm)
+      console.log(response)
     }
   })
 }
 
+/**
+ * 密码框状态切换方法
+ */
 const handllePassWordStatus = () => {
   inputType.value = inputType.value === 'password' ? 'text' : 'password'
 }
+
 </script>
+
 <style scoped lang="scss">
 $bg: #2d3a4b;
 $dark_gray: #889aa4;
 $light_gray: #eee;
 $cursor: #fff;
 
-.login-container {
+.login-container{
   position: relative;
   height: 100%;
-  background-color: $bg;
+  background-color:$bg;
 
-  .login-form {
+  .login-form{
     width: 520px;
     padding: 0 35px;
-    position: absolute;
-    left: 50%;
-    margin-left: -260px;
-    top: 160px;
+    position :absolute;
+    left : 50%;
+    margin-left : -260px;
+    top : 160px;
     overflow: hidden;
 
-    .el-form-item {
+    ::v-deep .el-form-item{
       border: 1px solid rgba(255, 255, 255, 0.1);
       background: rgba(0, 0, 0, 0.1);
       border-radius: 5px;
       color: #454545;
 
-      .svg-container {
+      .svg-container{
         padding: 6px 5px 6px 15px;
         color: $dark_gray;
         vertical-align: middle;
         display: inline-block;
       }
+
+      .svg-pwd{
+        position: absolute;
+        right: 20px;
+        top: 10px;
+        font-size: 16px;
+        color: $dark_gray;
+        cursor: pointer;
+        user-select: none;
+      }
     }
 
-    .el-input {
-      display: inline-block;
-      height: 47px;
-      width: 85%;
-      .el-input__wrapper {
-        background: transparent !important;
-        box-shadow: none;
-      }
-      .el-input__wrapper.is-focus {
-        box-shadow: none;
-      }
-      input {
-        border: 0px;
-        -webkit-appearance: none;
-        border-radius: 0px;
-        padding: 12px 5px 12px 15px;
-        color: $light_gray;
+    ::v-deep .el-input {
+        display: inline-block;
         height: 47px;
-        caret-color: $cursor;
+        width: 85%;
+        .el-input__wrapper{
+          background: transparent!important;
+          box-shadow: none;
+        }
+        .el-input__wrapper.is-focus{
+          box-shadow: none;
+        }
+        input {
+          border: 0px;
+          -webkit-appearance: none;
+          border-radius: 0px;
+          padding: 12px 5px 12px 15px;
+          color: $light_gray;
+          height: 47px;
+          caret-color: $cursor;
+        }
       }
-    }
 
-    .title-container {
+    .title-container{
       position: relative;
 
-      .title {
+      .title{
         font-size: 26px;
-        color: $light_gray;
+        color : $light_gray;
         text-align: center;
         font-weight: bold;
-        margin-bottom: 40px;
+        margin-bottom : 40px;
       }
-      .icon {
-        position: absolute;
-        top: 0;
-        right: 0;
-        color: $light_gray;
-        font-size: 22px;
+      ::v-deep .svg-language{
+          position: absolute;
+          top: 4px;
+          right: 0;
+          background-color: #fff;
+          font-size: 22px;
+          padding: 4px;
+          border-radius: 4px;
+          cursor: pointer;
       }
     }
 
-    .login-button {
-      width: 100%;
-      margin-bottom: 30px;
+    .login-button{
+      width : 100%;
+      margin-bottom : 30px;
     }
   }
 }
+
 </style>
